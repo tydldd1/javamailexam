5ad3c2f55ba605f2e3ddff06b256179764f7a4ed
tydldd <ru2013_1@163.com>
2013-11-21 09:57:01 +0800

    发送邮件

---
+++ src/com/util/mail/SendMailTask.java
@@ -0,0 +1,36 @@
+package com.util.mail;
+
+import java.util.TimerTask;
+
+/**
+ * Description:
+ * User: NanChengRu
+ * Date: 13-11-20
+ * Time: 下午9:23
+ * JDK: 1.6
+ * version: 1.0
+ */
+public class SendMailTask extends TimerTask{
+    /**
+     * The action to be performed by this timer task.
+     */
+    @Override
+    public void run() {
+        //这个类主要是设置邮件
+        MailSenderInfo mailInfo = new MailSenderInfo();
+        mailInfo.setMailServerHost("smtp.163.com");
+        mailInfo.setMailServerPort("25");
+        mailInfo.setValidate(true);
+        mailInfo.setUserName("ru2013_1@163.com");
+        mailInfo.setPassword("195589");//您的邮箱密码
+        mailInfo.setFromAddress("ru2013_1@163.com");
+        mailInfo.setToAddress("1042391443@qq.com");
+        mailInfo.setSubject("设置邮箱标题");
+        mailInfo.setContent("test!!!!!!!!!");
+        //这个类主要来发送邮件
+        SimpleMailSender sms = new SimpleMailSender();
+        //sms.sendTextMail(mailInfo);//发送文体格式
+        boolean isSend = sms.sendHtmlMail(mailInfo);//发送html格式
+    }
+
+}
--- src/com/util/mail/Test.java
+++ src/com/util/mail/Test.java
@@ -1,5 +1,7 @@
 package com.util.mail;
 
+import java.util.Timer;
+
 /**
  * Description:
  * User: NanChengRu
@@ -10,21 +12,8 @@ package com.util.mail;
  */
 public class Test {
     public static void main(String[] args){
-        //这个类主要是设置邮件
-        MailSenderInfo mailInfo = new MailSenderInfo();
-        mailInfo.setMailServerHost("smtp.163.com");
-        mailInfo.setMailServerPort("25");
-        mailInfo.setValidate(true);
-        mailInfo.setUserName("ru2013_1@163.com");
-        mailInfo.setPassword("195589");//您的邮箱密码
-        mailInfo.setFromAddress("ru2013_1@163.com");
-        mailInfo.setToAddress("1042391443@qq.com");
-        mailInfo.setSubject("设置邮箱标题");
-        mailInfo.setContent("test!!!!!!!!!");
-        //这个类主要来发送邮件
-        SimpleMailSender sms = new SimpleMailSender();
-        //sms.sendTextMail(mailInfo);//发送文体格式
-        sms.sendHtmlMail(mailInfo);//发送html格式
+        Timer timer = new Timer();
+        timer.schedule(new SendMailTask(),1000,5000);
     }
 
 }
